9b890a763e33509796f13f59de3bac85fbf56870
BBakerpkt <bbakerpkt@gmail.com>
2012-04-16 09:22:49 -0700

    fixing signup

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,12 +1,11 @@
 module SessionsHelper
 
   def sign_in(user)
-    cookies.permanent.signed[:remember_token] = [user.id, user.salt]
+    cookies.signed[:remember_token] = [user.id, user.salt]
     self.current_user = user
   end
-
-  def current_user
-    @current_user ||= user_from_remember_token
+  def current_user=(user)
+	@current_user = user
   end
 
   def signed_in?
